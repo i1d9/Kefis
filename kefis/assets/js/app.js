bbf92b3b704655166f8bc7103b5c47501477525d
ian <highmeme10@gmail.com>
2022-02-15 23:32:30 +0300

    Added Bootstrap

--- kefis/assets/js/app.js
+++ kefis/assets/js/app.js
@@ -1,6 +1,5 @@
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
-import "../css/app.css"
 
 // If you want to use Phoenix channels, run `mix help phx.gen.channel`
 // to get started and then uncomment the line below.
@@ -23,6 +22,7 @@ import "../css/app.css"
 import "phoenix_html"
 
 import 'bootstrap'; // Bootstrap support
+import "jquery";
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
--- kefis/config/dev.exs
+++ kefis/config/dev.exs
@@ -25,7 +25,13 @@ config :kefis, KefisWeb.Endpoint,
   secret_key_base: "ZMalbnhl9sRAnuPQwmoBnQ/5KCDrwGebfRT559SA8bqcXRChgpgfIboCQvzJkhwk",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    sass: {
+      DartSass,
+      :install_and_run,
+      [:default, ~w(--embed-source-map --source-map-urls=absolute --watch)]
+  }
+
   ]
 
 # ## SSL Support
