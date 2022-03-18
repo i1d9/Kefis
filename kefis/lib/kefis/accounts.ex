71e55ac7db4ac0971a2f07d37aeb619908b29374
ian <highmeme10@gmail.com>
2022-03-19 00:19:34 +0300

    Associate a Transaction with an account init

--- kefis/lib/kefis/accounts.ex
+++ kefis/lib/kefis/accounts.ex
@@ -2,7 +2,8 @@ defmodule Kefis.Accounts do
 
   alias Kefis.Chain.Account
   alias Kefis.Repo
-
+  alias Kefis.Chain.Transaction
+  alias Ecto.Changeset
 
   def create(user,details) do
     Account.changeset(%Account{}, details)
@@ -23,6 +24,12 @@ defmodule Kefis.Accounts do
   def withdraw(%Account{} = account, amount) do
     Account.changeset(account, %{balance: account.balance - amount})
     |> Repo.update()
+  end
+
+  def transact(account, type, amount, details) do
+    Transaction.changeset(%Transaction{}, details)
+    |> Changeset.put_assoc(:account, account)
+    |> Repo.insert!()
   end
 
 
