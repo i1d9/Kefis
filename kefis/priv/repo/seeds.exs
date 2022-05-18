e34e2adcf8ff6c08040991617b06f21e3d2edde2
ian <highmeme10@gmail.com>
2022-05-18 11:30:30 +0300

    Retailer Seed

--- kefis/priv/repo/seeds.exs
+++ kefis/priv/repo/seeds.exs
@@ -21,7 +21,7 @@ alias Kefis.Chain.{
   DispatchDetails,
   Order,
   OrderDetail,
-  Transaction,iex
+  Transaction,
   Warehouse
 }
 
@@ -29,8 +29,8 @@ alias Ecto.Changeset
 
 # Create a Super User Account
 super_user = %{
-  first_name: "User",
-  second_name: "Zero",
+  first_name: "John",
+  second_name: "Doe",
   phone: "254712345418",
   email: "admin@gmail.com",
   role: "super",
@@ -162,4 +162,127 @@ for supplier_details <- suppliers do
     |> Repo.insert!()
 
   IO.inspect(supplier)
+end
+
+retailers = [
+  %{
+    email: "popup_micro@retailer.com",
+    lat: -1.262093,
+    lng: 36.833749,
+    location: "Kiambu",
+    phone: "2542051455",
+    type: "retailer",
+    name: "Mirco Pop-Up"
+  },
+  %{
+    email: "expressstores@retailer.com",
+    lat: -1.261899,
+    lng: 36.825721,
+    location: "Mtama Road",
+    phone: "2540052491",
+    type: "retailer",
+    name: "Express Stores"
+  },
+  %{
+    email: "nuvita@retailer.com",
+    lat: -1.258606,
+    lng: 36.835578,
+    location: "Serengeti Avenue",
+    phone: "25412051455",
+    type: "retailer",
+    name: "Eastmatt Stores"
+  },
+  %{
+    email: "buyall@retailer.com",
+    lat: -1.244324,
+    lng: 36.856212,
+    location: "Pipeline Estate Road",
+    phone: "2544057112",
+    type: "retailer",
+    name: "Buy All Kenya"
+  },
+  %{
+    email: "valleysupermarket@retailer.com",
+    lat: -1.260236,
+    lng: 36.856435,
+    location: "Utalii Lane",
+    phone: "2547307000",
+    type: "retailer",
+    name: "Kahawa Valley Supermarket"
+  },
+  %{
+    email: "cleansupermkt@retailer.com",
+    lat: -1.238864,
+    lng: 36.855890,
+    location: "Coffee Garden Road",
+    phone: "2540651256",
+    type: "retailer",
+    name: "Cleanshelf Supermarket LTD"
+  },
+  %{
+    email: "bestprice@retailer.com",
+    lat: -1.317246,
+    lng: 36.823211,
+    location: "Kagondo Road",
+    phone: "25401021883",
+    type: "retailer",
+    name: "Best Price Shop Factory"
+  },
+  %{
+    email: "dukasmart@retailer.com",
+    lat: -1.315410,
+    lng: 36.836455,
+    location: "Zanzibar Road",
+    phone: "2542065125",
+    type: "retailer",
+    name: "Smart Duka LTD"
+  },
+  %{
+    email: "malian@retailer.com",
+    lat: -1.314499,
+    lng: 36.838291,
+    location: "Kapiti Road",
+    phone: "25420651251",
+    type: "retailer",
+    name: "Malian Limited"
+  }
+]
+
+"""
+Tried Ecto.Multi.new() but it doesn't work 🙂 I'm justifiying the loop
+"""
+
+for retailer_details <- retailers do
+  names = String.split(retailer_details.name, " ")
+
+  retailer_user = %{
+    first_name: Enum.at(names, 0),
+    second_name: Enum.at(names, 1),
+    phone: retailer_details.phone,
+    role: "retailer_admin",
+    email: retailer_details.email,
+    password: retailer_details.email,
+    password_confirmation: retailer_details.email
+  }
+
+  retailer_user = User.admin_changeset(%User{}, retailer_user)
+  retailer_user = Repo.insert!(retailer_user)
+
+  retailer =
+    retailer_user
+    |> Ecto.build_assoc(:partner)
+    |> Partner.changeset(retailer_details)
+    |> Repo.insert!()
+
+  retailer_account_details = %{
+    balance: 0,
+    status: "active"
+  }
+
+  retailer_money_account =
+    Account.changeset(%Account{}, retailer_account_details)
+    |> Ecto.Changeset.put_assoc(:partner, retailer)
+    |> Repo.insert!()
+
+  IO.inspect(retailer)
 end
